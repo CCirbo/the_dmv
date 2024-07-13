ef5522edea98065dfcd9501c012c2b9e1d13f9e1
Candice Cirbo <thecirbos@icloud.com>
2024-07-12 21:08:38 -0600

    Adds passing tests for all methods facility class

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe Facility do
       expect(@facility.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
       expect(@facility.phone).to eq('(720) 865-4600')
       expect(@facility.services).to eq([])
+      
     end
   end
 
@@ -34,21 +35,72 @@ RSpec.describe Facility do
     end
 
     it 'can add available services to another facility' do
-      expect(@facility_1.add_service('Vehicle Registration')).to eq(["Vehicle Registration"])
+      @facility_1.add_service('New Drivers License')
+      @facility_1.add_service('Vehicle Registration')
+      expect(@facility_1.services).to eq(['New Drivers License', 'Vehicle Registration'])
+
+      @facility_2.add_service('New Drivers License')
+      @facility_2.add_service('Renew Drivers License')
+      expect(@facility_2.services).to eq(['New Drivers License','Renew Drivers License'])
     end
   end
 
   describe '#vehicles services' do
-    it 'it has registered vehicles in an array' do
-      expect(@facility_1.registered_vehicles).to eq ([])
+    before(:each) do
+      @facility_1.add_service('Vehicle Registration')
     end
 
-    it 'can collect vehicle registration fees' do
-      expect(@facility_1.collected_fees).to eq(0)
-    end
-
-    it 'can register vehicles' do
+    it 'it can register vehicles of any kind' do
+      expect(@facility_1.registered_vehicles).to eq([])
       expect(@facility_1.register_vehicle(@cruz)).to eq([@cruz])
+      expect(@facility_1.registered_vehicles).to eq([@cruz])
+      expect(@facility_1.register_vehicle(@camaro)).to eq([@cruz, @camaro])
+      expect(@facility_1.register_vehicle(@bolt)).to eq([@cruz, @camaro, @bolt])
+      expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro, @bolt])
     end
+
+    it 'can update registration date when vehicle is registered' do
+      @facility_1.register_vehicle(@cruz)
+      expect(@cruz.registration_date).to eq(Date.today)
+
+      @facility_1.register_vehicle(@camaro)
+      expect(@camaro.registration_date).to eq(Date.today)
+
+      @facility_1.register_vehicle(@bolt)
+      expect(@bolt.registration_date).to eq(Date.today)
+    end
+
+    it 'assigns a license plate based on car type when registered' do
+      @facility_1.register_vehicle(@cruz)
+      expect(@cruz.plate_type).to eq(:regular)
+
+      @facility_1.register_vehicle(@camaro)
+      expect(@camaro.plate_type).to eq(:antique)
+
+      @facility_1.register_vehicle(@bolt)
+      expect(@bolt.plate_type).to eq(:ev)
+    end
+
+    it 'can collect vehicle registration fees for all vehicles' do
+      expect(@facility_1.collected_fees).to eq(0)
+      @facility_1.register_vehicle(@cruz)
+      expect(@facility_1.collected_fees).to eq(100)
+
+      @facility_1.register_vehicle(@camaro)
+      @facility_1.register_vehicle(@bolt)
+      expect(@facility_1.collected_fees).to eq(325)
+    end
+
+    it 'cannot register any vehicles if services are not offered' do
+      expect(@facility_2.registered_vehicles).to eq([])
+      expect(@facility_2.services).to eq([])
+      expect(@facility_2.register_vehicle(@bolt)).to eq(nil)
+      expect(@facility_2.registered_vehicles).to eq([])
+      expect(@facility_2.collected_fees).to eq(0)
+    end
+
+    
   end
 end
+
+
